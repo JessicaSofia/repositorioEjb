008596d4f9cc3e3293e00261a18889087762188b
margp_th20 <marpilacuan@gmail.com>
2018-07-13 07:24:40 -0500

    Cambios liencias

--- ejbModule/ec/edu/uce/controlAsistencia/ejb/servicios/interfaces/LicenciaServicio.java
+++ ejbModule/ec/edu/uce/controlAsistencia/ejb/servicios/interfaces/LicenciaServicio.java
@@ -2,32 +2,32 @@ package ec.edu.uce.controlAsistencia.ejb.servicios.interfaces;
 
 import java.util.List;
 
-import ec.edu.uce.controlAsistencia.jpa.entidades.LicenciaYPermiso;
+import ec.edu.uce.controlAsistencia.jpa.entidades.Licencia;
 
 
-public interface LicenciaPermisoServicio { 
+public interface LicenciaServicio { 
 	
 	
 	/***
 	 * Inserta un objeto de Tipo LicenciaYPermiso
-	 * @param licenciaPermiso
+	 * @param licencia
 	 * @return
 	 */
-	public boolean  LicenciaPermisoInsertar(LicenciaYPermiso licenciaPermiso);
+	public boolean  LicenciaInsertar(Licencia licencia);
 	
 	/***
 	 * Obtiene una lista de Tipo LicenciaPermiso Por DetallePuestoId
 	 * @param detallePuestoId
 	 * @return
 	 */
-	public List<LicenciaYPermiso > ListaLicenciaYPermisoPorDetallePuestoId(int  detallePuestoId );
+	public List<Licencia > ListaLicenciaPorDetallePuestoId(int  detallePuestoId );
 	
 	/***
 	 * Actualiza un objet de Tipo licenciaYPermiso
 	 * @param licenciaYPermiso
 	 * @return
 	 */
-	public LicenciaYPermiso LicenciaYPermisoActualizar(LicenciaYPermiso licenciaYPermiso );
+	public Licencia LicenciaActualizar(Licencia licencia);
 	
 	 /***
 	  * Obtiene el Numero de Autorizacion de una licenciaYPermio
